4abfec3e57c9f2a84a634d25ce186c712e74ba90
matthew633jdi <matthew633jdi@gmail.com>
2026-01-30 00:07:52 +0900

    feat: Implement common API response wrapper

---
+++ src/main/java/com/dailyseed/task_manager/global/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.dailyseed.task_manager.global;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBadRequest(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.fail(e.getMessage()));
+    }
+}
